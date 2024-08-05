d7fa90ed6a5395410db1f27307cb4dc68a3d75f2
Mike Mambwe <mikeapple056@gmail.com>
2024-08-05 15:25:56 +0300

    the final

--- src/Count.jsx
+++ src/Count.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Count = (props) => {
   return (
     <div>
-      <p>Number presseded is {props.count}</p>
+      <p>
+        Number presseded is <strong>{props.count}</strong>
+      </p>
     </div>
   );
 }
