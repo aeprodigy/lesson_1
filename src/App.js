5f4ad8021a7499af1875cfc55bb64d5fea83ba96
Mike Mambwe <mikeapple056@gmail.com>
2024-08-05 15:22:27 +0300

    React-resources

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Count from './Count';
 
 function App() {
+
+  const [advice, setAdvice ]= useState(null);
+//the state to update the number of times the user pressed at the getAdvice button.
+const [count, setCount] = useState(0);
+
+  const getAdvice = async ()  =>{
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip)
+    console.log(data.slip.advice);
+    setCount((counter) => count + 1)
+  }
+  useEffect(function (){
+    getAdvice()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>The ADVICE APP: react js</div>
+      <button onClick={getAdvice}>Get Advice</button>
+      <div>
+        <h1>Today's advice</h1>
+        
+        <Count count={count}/>
+        {advice ? (
+          <p key={advice.id}>{advice.advice}</p>
+        ):<p>NO advice Yet!</p>}
+      </div>
     </div>
   );
 }
---
+++ src/Count.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Count = (props) => {
+  return (
+    <div>
+      <p>Number presseded is {props.count}</p>
+    </div>
+  );
+}
+
+export default Count
